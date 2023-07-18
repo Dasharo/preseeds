10a893084038d1c103aa51c67c6bd27bd974f3ed
Mixss <michal.ziemiec@3mdeb.com>
2023-07-18 15:53:06 +0200

    windows/setup-ssh.ps1: add automatic ssh service start

--- windows/setup-ssh.ps1
+++ windows/setup-ssh.ps1
@@ -3,5 +3,8 @@ Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Get-Service -Name ssh-agent | Set-Service -StartupType Manual
 Start-Service ssh-agent
+Get-Service -Name ssh-agent | Set-Service -StartupType Automatic
 Start-Service sshd
+Get-Service -Name sshd | Set-Service -StartupType Automatic
 New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force

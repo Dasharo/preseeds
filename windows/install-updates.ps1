9e6691eabbeb126c7e07526783c0b0ec569ea912
Mixss <michal.ziemiec@3mdeb.com>
2023-07-19 08:45:27 +0200

    windows/install-updates.ps1: always try to install PSGallery repository

--- windows/install-updates.ps1
+++ windows/install-updates.ps1
@@ -1,7 +1,6 @@
 Write-Host "Downloading all Windows updates...";
 Install-PackageProvider NuGet -Force;
-# Uncomment the following line when the installation was performed without internet connection (PSGallery repository was not installed correctly)
-# Register-PSRepository -Default -Verbose
+Register-PSRepository -Default -Verbose
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted;
 Install-Module PSWindowsUpdate;
 Get-WindowsUpdate -AcceptAll -Install -AutoReboot

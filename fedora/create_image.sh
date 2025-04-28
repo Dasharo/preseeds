086ab3d923a1fa3ca26cd132dde8c3d99f48d72c
Filip Gołaś <filip.golas@3mdeb.com>
2025-04-28 17:45:09 +0200

    fedora/create_image.sh: Clean the script up from unused variables
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

--- fedora/create_image.sh
+++ fedora/create_image.sh
@@ -6,23 +6,16 @@ FEDORA_MAJOR_VERSION="42"
 FEDORA_MINOR_VERSION="1.1"
 FEDORA_VERSION=${FEDORA_MAJOR_VERSION}-${FEDORA_MINOR_VERSION}
 ISO_DOWNLOAD_LINK="https://download.fedoraproject.org/pub/fedora/linux/releases/${FEDORA_MAJOR_VERSION}/Everything/x86_64/iso/Fedora-Everything-netinst-x86_64-${FEDORA_VERSION}.iso"
-SCRIPTDIR=$(readlink -f $(dirname "$0"))
-PARTITIONING_KS="$SCRIPTDIR/partitioning.cfg"
-MAIN_KS="$SCRIPTDIR/main.cfg"
 OUTPUT_ISO="fedora-auto-${FEDORA_VERSION}.iso"
 ISO_PATH="${ISO_PATH:-fedora.iso}"
-ISO_EXTR_PATH=""
-PARTITIONING=1
 
-while getopts "hi:e:o:p" arg; do
+while getopts "hi:o" arg; do
     case "${arg}" in
         h)
             echo "This script builds a custom Fedora ISO using a Kickstart file."
             echo "Help:"
             echo "    -h            shows help"
-            echo "    -p            disable custom partitioning (omit partitioning.ks)"
             echo "    -i path       use a specific Fedora ISO file"
-            echo "    -e path       use an extracted ISO directory instead of extracting"
             echo "    -o file_name  output ISO filename"
             exit 0
             ;;
@@ -34,19 +27,8 @@ while getopts "hi:e:o:p" arg; do
                 ISO_PATH=$OPTARG
             fi
             ;;
-        e)
-            if ! [ -d "$OPTARG" ]; then
-                echo "Directory $OPTARG does not exist"
-                exit 2
-            else
-                ISO_EXTR_PATH=$OPTARG
-            fi
-            ;;
         o)
             OUTPUT_ISO=$OPTARG
-            ;;
-        p)
-            PARTITIONING=0
             ;;
         *)
             echo "Unrecognized argument. Use -h to get help"
@@ -55,20 +37,12 @@ while getopts "hi:e:o:p" arg; do
     esac
 done
 
-# check prerequisities
-if ! xorriso --version &> /dev/null
-then
-    echo "xorriso could not be found"
-    exit 4
-fi
 
 # download iso image
 if [[ ! -f "$ISO_PATH" ]]; then
     echo "Downloading Fedora Workstation ${FEDORA_VERSION} image..."
     wget -O "$ISO_PATH" $ISO_DOWNLOAD_LINK
 fi
-
-echo "Saving modified iso..."
 
 # The iso is a hybrid el-torrito iso and the EFI partition is the second
 # partition. Using xorriso '-map' won't work, because it only modifies the
